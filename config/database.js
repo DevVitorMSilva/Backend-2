8357142a01717904bc13ef4bdbddec51fc8e5535
Vitor Silva <vitormarcos2005@hotmail.com>
2025-01-21 12:35:33 -0300

    Corrige configuração do Sequelize para usar variáveis de ambiente com dotenv

--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
-    dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+    }
+);
 
-module.exports = sequelize
+module.exports = sequelize;
